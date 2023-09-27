faecc4ee7e5ccba8f32ed662e5b98e0a7924f139
farhanbin65 <farhanbin65@gmail.com>
2023-09-27 13:43:44 +0600

    pieChart added

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,11 +1,48 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-const-assign */
+import { useLoaderData } from 'react-router-dom';
+import { getStoredDonation } from '../Utility/localStorage';
+import { PieChart, Pie,Legend, Tooltip} from 'recharts';
+
+
 const Statistics = () => {
+    const donatedData = getStoredDonation();
+    const donateDataLength = donatedData.length;
+
+    const allDonations = useLoaderData();
+    const allDonationsLength = allDonations.length;
+
+    
+    const percentage1 = (donateDataLength/allDonationsLength)*100;
+    const percentage2 = (100 - percentage1);
+
+    const p1 = Math.round(percentage1);
+    const p2 = Math.round(percentage2);
+    
+    const data01 = [
+        { name: 'Total Donation', value: p1, fill:'#0088FE',},
+        { name: 'Your Donation', value: p2, fill:'#00C49F',},
+      ];
+  
+    // console.log(donateDataLength, allDonationsLength);
+    console.log(p1, p2);
     return (
-
-        
-
-        <div>
-            <h1>Could not make it</h1>
+         <div>
+            <PieChart width={600} height={600}>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data01}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+           label
+          />
+          <Legend></Legend>
+          <Tooltip />
+        </PieChart>
         </div>
+
     );
 };
 
